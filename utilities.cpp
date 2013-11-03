bf6b6752ea5530e5ea218815665a3909853c4ac3
Tom Renner <tr325@cam.ac.uk>
2013-11-03 04:13:09 +0000

    "added skeleton n-r root finding - currently unconvinced by results, needs thorough testing"

--- utilities.cpp
+++ utilities.cpp
@@ -1,6 +1,7 @@
 /* File containing utility functions for Neural Network simulations   */
 
 #include<iostream>
+#include<cmath>
 #include"utilities.h"
 
 using namespace std; 
@@ -11,116 +12,182 @@ extern "C" double dtrsyl_(char* trana, char* tranb, int* isgn, int* orda, int* o
 
 extern "C" double dgees_(char* jobvs, char* sort, bool* select, int* n, double* A, int* lda, int* sdim, double* wr, double* wi, double* vs, int* ldvs, double* work, int* lwork, bool* bwork, int* info);
 
-
+extern "C" double dgemm_(char* trana, char* tranb, int* m, int* n, int* k, double* alpha, double* A, int* lda, double* B, int* ldb, double* beta, double* C, int* ldc);
 
 
 /*  Takes inputs the required epsilon (larger->smoother SSA) and the input matrix A  */ 
 /*  Calculates the value of the SSA using the lyapunov eqn solver  functions  */ 
 double SimpleSSA(double *W[], double eps)
 {
+    double precision; 
+    precision = 0.000001;
     
     // Generate A = (W-sI) 
-    double *I[SIZE];
-    double *A[SIZE];
-    double *A_t[SIZE]; 
-      
+    double *I[SIZE], *A[SIZE], *A_t[SIZE];
+    double *fP, *fQ;
+    double *fA;
+    double *fA_t; 
+    double *P[SIZE], *Q[SIZE];
+    double spectralAbcissa;
+    double s, sOld;
+    int l, loopcount;
+    
+    l = 0;
+    loopcount = 0;
+    s = 0.00;
+    sOld = 0.01;
+          
     for(int i=0; i<SIZE; i++)
     {
         I[i] = new double[SIZE];
         A[i] = new double[SIZE];
         A_t[i]= new double[SIZE]; 
-        I[i][i] = 1;                // The rest of the array default initialises to 0
-        for(int j=0; j<SIZE; j++)
-        {
-            A[i][j] = W[i][j] - eps*I[i][j];
-        }
-    }
-    
-    cout << "Matrix A:" <<endl; 
-    for(int i=0; i<SIZE; i++)
-    {
-        for(int j=0; j<SIZE; j++)
-        {
-            cout << A[i][j] << "    ";
-        }
-        cout << endl; 
-    }
-    cout << endl;
-    
-    // Form transpose(A)
-    for(int i=0; i<SIZE; i++)
-    {
-        for(int j=0; j<SIZE; j++)
-        {
-            A_t[i][j] = A[j][i];
-        }
-    }     
-                
-    // convert A and A_t to Schur form using dgees_ 
-    double *fA;
-    double *fA_t; 
-    
-    fA = FArrayConvert(A, SIZE);
-    Schur(&fA);     
-    CArrayConvert(fA, A, SIZE); 
-    
-    fA_t = FArrayConvert(A_t, SIZE); 
-    Schur(&fA_t); 
-    CArrayConvert(fA_t, A_t, SIZE); 
-    
-    cout << endl << "Schur(A): " << endl;
-     for(int i=0; i<SIZE; i++)
-    {
-        for(int j=0; j<SIZE; j++)
-        {
-            cout << A[i][j] << "    ";
-        }
-        cout << endl; 
-    }
-    cout << endl;
-    
-    // Solve lyapunov equations for P and Q 
-    double *fP, *fQ;
-    double *P[SIZE], *Q[SIZE];
-    for(int i=0; i<SIZE; i++)
-    {
         P[i] = new double[SIZE];
         Q[i] = new double[SIZE];
+        I[i][i] = 1;                // The rest of the array default initialises to 0
     }
+    
+    
+    // main loop for N-R root finding method
+    while(abs(s - sOld) > precision)
+    {
+        sOld = s; 
+        
+        for(int i=0; i<SIZE; i++)
+        {
+            for(int j=0; j<SIZE; j++)
+            {
+                A[i][j] = W[i][j] - s*I[i][j];
+            }
+        }
+        // Form transpose(A)
+        for(int i=0; i<SIZE; i++)
+        {
+            for(int j=0; j<SIZE; j++)
+            {
+                A_t[i][j] = A[j][i];
+            }
+        }     
+                    
+        // convert A and A_t to Schur form using dgees_ 
+        fA = FArrayConvert(A, SIZE);
+        Schur(&fA);     
+        CArrayConvert(fA, A, SIZE); 
+        
+        fA_t = FArrayConvert(A_t, SIZE); 
+        Schur(&fA_t); 
+        CArrayConvert(fA_t, A_t, SIZE); 
+        
+        // Find SA value (max value of diagonals of Schur form) 
+        // operates on first loop only
+        if(l == 0)
+        {
+            spectralAbcissa = MaxDiag(A, SIZE);
+            cout << "Spectral abcissa value = " << spectralAbcissa <<endl; 
+            l++;
+        }            
+        
+        // Solve lyapunov equations for P and Q    
+        fP = Lyap(fA, fA_t); 
+        CArrayConvert(fP, P, SIZE); 
+      
+        fQ = Lyap(fA_t, fA); 
+        CArrayConvert(fQ, Q, SIZE); 
+       
+        // use Newton-Raphson method to find new s value 
+        s = Newton(Q, P, eps, spectralAbcissa);  
+        
+        cout << "s = " << s << ", sOld = " << sOld <<endl;
+        loopcount++;
+    }   
+    
+    cout << endl << "Smoothed spectral abcissa value = " << s << endl; 
+    cout << "Loop ran " << loopcount << " times" <<endl; 
+    return s; 
+}
+
+/*  Finds the value of s for the next iteration of the loop     */
+double Newton(double *Q[], double *P[], double eps, double sA)
+{
+    double val, grad, e, s; 
+    double *fP, *fQ, *fPQ;
+    double *PQ[SIZE]; 
+    
+    for(int i=0; i<SIZE; i++)
+    {
+        PQ[i] = new double[SIZE];
+    }    
+    
+    fP = FArrayConvert(P, SIZE); 
+    fQ = FArrayConvert(Q, SIZE); 
+    fPQ = FArrayConvert(PQ, SIZE);
+    MatMult(fP, fQ, fPQ); 
+    CArrayConvert(fPQ, PQ, SIZE); 
+    
+    val = Trace(Q, SIZE); 
+    e = 1/eps;
+    grad = -2*Trace(PQ, SIZE); 
+    s = (1/grad)*(e - val); 
      
-    fP = Lyap(fA, fA_t);
- //   for(int i=0; i<(SIZE*SIZE); i++)
- //       cout << fP[i] << endl; 
-    CArrayConvert(fP, P, SIZE); 
     
-    cout << "Matrix P:" <<endl; 
-    for(int i=0; i<SIZE; i++)
+    if(s < sA)
     {
-        for(int j=0; j<SIZE; j++)
-        {
-            cout << P[i][j] << "    ";
-        }
-        cout << endl; 
+        s = sA + eps*eps*eps*eps; 
     }
-    cout << endl;
+    cout << "gradient = " <<grad <<", s = " << s << endl;  
     
-    fQ = Lyap(fA_t, fA); 
-    CArrayConvert(fQ, Q, SIZE); 
+    return s; 
+}    
+
+/*  Performs matrix multiplication of matrices A and B,     
+ *  storing the result in C                                 */
+void MatMult(double A[], double B[], double C[])
+{
+    char TRANA, TRANB; 
+    int M, N, K, LDA, LDB, LDC; 
+    double ALPHA, BETA; 
     
-    cout << "Matrix Q:" <<endl; 
-    for(int i=0; i<SIZE; i++)
+    TRANA = 'N';
+    TRANB = 'N';
+    M = SIZE;
+    N = SIZE;
+    K = SIZE;
+    LDA = SIZE;
+    LDB = SIZE;
+    LDC = SIZE;
+    ALPHA = 1.00;
+    BETA = 0.00;
+    
+    dgemm_(&TRANA, &TRANB, &M, &N, &K, &ALPHA, A, &LDA, B, &LDB, &BETA, C, &LDC);
+    
+    return;
+}
+    
+    
+/*  Calculates the trace of a matrix                    */
+double Trace(double *A[], int dimA)
+{
+    double t = 0.00; 
+    for(int i=0; i<dimA; i++)
     {
-        for(int j=0; j<SIZE; j++)
-        {
-            cout << Q[i][j] << "    ";
-        }
-        cout << endl; 
+        t += A[i][i];
     }
-    cout << endl;
+    return t;
+}
+
+/*  Finds the maximum value of the diagonals of a matrix    */
+double MaxDiag(double *A[], int dimA)
+{
+    double p = 0.00;
+    for(int i=0; i<dimA; i++)
+    {
+        if(A[i][i] > p)
+        {
+            p = A[i][i];
+        }
+    }
     
-    
-    // use Newton-Raphson method to locate SSA   
-    
+    return p; 
 }
 
 /*  Converts input array to Column-major 1-D array for      */
@@ -217,7 +284,7 @@ double* Lyap(double A[], double B[])
     
     dtrsyl_(&TRANA, &TRANB, &ISGN, &ORDA, &ORDB, A, &LDA, B, &LDB, fC, &LDC, &SCALE, &INFO);  
     
-    cout << "dtrsyl return status: " <<INFO << endl; 
+    //cout << "dtrsyl return status: " <<INFO << endl; 
     
     return fC;
 }
--- utilities.h
+++ utilities.h
@@ -1,6 +1,25 @@
 
 const int SIZE = 3;
 
+
+/*  Performs matrix multiplication of matrices A and B,     
+ *  storing the result in C                                 */
+void MatMult(double A[], double B[], double C[]);
+
+
+/*  Finds the value of the smoothed spectral abcissa using  */
+/*  the Newton-Raphson root-finding technique               */
+double Newton(double *Q[], double *P[], double eps, double sA);
+
+
+/*  Calculates the trace of a matrix                    */
+double Trace(double *A[], int dimA);
+
+
+/*  Finds the maximum value of the diagonals of a matrix    */
+double MaxDiag(double *A[], int dimA);
+
+
 /* Solves lyapunov eqn for 0 = L(P,A,U,s)   */ 
 /* Returns pointer to P                     */
 double* Lyap(double A[], double B[]);
